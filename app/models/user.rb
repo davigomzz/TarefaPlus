3e6d6c0c27fd89273b675cfa6b6564ae30d35798
Davi gomes <davigomesdev@gmail.com>
2025-02-09 21:35:53 -0300

    fix: resolvendo erros da navbar, logout e current user e adicionando método para transformar o nome em sempre primeira letra maiúscula e exibir sempre o primeiro nome

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
-  
   has_many :tasks, dependent: :destroy
-
   validates :email, presence: true, uniqueness: true
+  before_validation :capitalize_name
+
+  private
+
+  def capitalize_name
+    self.name = name.capitalize if name.present?
+  end
 end
