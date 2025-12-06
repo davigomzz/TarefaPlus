7188c8e1398f950c00e91d9a9170936f4f3b5196
Davi gomes <davigomesdev@gmail.com>
2025-12-06 11:23:54 -0300

    enhancement: change the actual setup to use  xray (jQuery) and bootstrap via importmap

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link jquery3.js
+//= link xray.js
+//= link xray.css
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
-pin 'application'
+pin "application"
 
-pin 'bootstrap', to: 'bootstrap.min.js', preload: true
-pin '@popperjs/core', to: 'popper.js', preload: true
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.3/dist/js/bootstrap.esm.js", preload: true
